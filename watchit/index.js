fb87c30f391dbc51e6d53c9edbcf837888cb8795
Ondrej Macoszek <omacoszek1@gmail.com>
2023-01-09 17:12:33 +0100

    backup commit

---
+++ watchit/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const chokidar = require("chokidar");
+
+chokidar
+  .watch(".")
+  .on("add", () => {
+    console.log("file added");
+  })
+  .on("change", () => {
+    console.log("file changed");
+  })
+  .on("unlink", () => {
+    console.log("file unlinked");
+  });

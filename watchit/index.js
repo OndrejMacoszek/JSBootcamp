1a6527af84c6146ebb90d8bccd9a22d7aab22cd3
Ondrej Macoszek <omacoszek1@gmail.com>
2023-01-16 22:27:56 +0100

    finish watchit version

--- watchit/index.js
+++ watchit/index.js
@@ -3,17 +3,36 @@
 const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
 const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
-const start = debounce(() => {
-  console.log("starting users program");
-},100);
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of an file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => {
-    console.log("file changed");
-  })
-  .on("unlink", () => {
-    console.log("file unlinked");
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(">>>> Starting process...");
+
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
   });
+
+program.parse(process.argv);
